2148bc11250672e8c306706271c9a706235450fe
BuildTools <unconfigured@null.spigotmc.org>
2020-10-20 14:09:42 +0200

    Redid the input handling
    * Improved how input is handled and processed
    * Eliminated all the input processing bugs (I think)
    * Added settings
    * Improved mobile compatibility

--- main.js
+++ main.js
@@ -8,16 +8,21 @@ timerStarted = false;
 incorrectLetterColor = "indianred";
 correctLetterColor = "lightgreen";
 
-mistakesAllowed = true;
-wordsListLevel=0;    //0-easy 1-hard
-wordListsList=[wordListEasy,wordListHard]
+eval(document.cookie.split('set_').join('')) // this sets all the settings from a existing cookies
+
+// mistakesAllowed = true;
+// wordListLevel = 0;    //0-easy 1-hard
+wordListsList = [wordListEasy, wordListHard]
+
+
 
 function generateWords() {
 	newText = "";
+	textIn.value = ''
 	wordsArray = [];
 	currentWordNum = 0;
 	currentWordLetterNum = 0;
-	wordList=wordListsList[wordsListLevel];
+	wordList = wordListsList[wordListLevel];
 	for (let i = 0; i < wordCount; i++) {
 		randomWordNum = Math.round(Math.random() * wordList.length);
 		wordsArray.push(wordList[randomWordNum]);
@@ -31,54 +36,41 @@ function generateWords() {
 	}
 	textToType.innerHTML = newText;
 	document.querySelector(`word${currentWordNum}`).style.color = "inherit";
-	textIn.style.backgroundColor="";
+	textIn.style.backgroundColor = "";
 	// setTimeout(() => {
 	// }, 1);
 }
 
 function addListeners() {
-	textIn.addEventListener("keydown", (event) => {
-		if (event.keyCode == 8) {
-			textInVal = textIn.value.slice(0, -1);
-			letterInVal = textInVal.slice(-1); // Get the last letter of the typed word after backspace has been accounted for.
-			
-			if (currentWordLetterNum != 0) {
-				document.querySelector(`word${currentWordNum}`).style.borderRightWidth = "0px";
-				currentWordLetterNum = textInVal.length;
-
-				if (!event.ctrlKey) {
-					currentLetterElement = getWordLetter(currentWordNum, currentWordLetterNum);
-					currentLetterElement.style.color = "inherit";
-				} else {
-					wordLettersElements = document.querySelector(`word${currentWordNum}`).children;
-					for (let i = 0; i < wordLettersElements.length; i++) {
-						wordLettersElements[i].style.color = "inherit";
-						currentWordLetterNum = 0;
-						textIn.value = "";
-					}
-					textIn.style.backgroundColor="";
-				}
-				console.log(textInVal,wordsArray[currentWordNum]);
-				if (textInVal==wordsArray[currentWordNum].slice(0,textInVal.length)) {
-					textIn.style.backgroundColor="";
-				}
-			}
-		}
-	});
-
-	textIn.addEventListener("keypress", (event) => {
-		if (event.keyCode == 13) {
-			event.stopPropagation();
-			event.preventDefault();
-			return false;
-		}
+	textIn.addEventListener("input", (event) => {
 		currentLetterElement = getWordLetter(currentWordNum, currentWordLetterNum);
-		letterInVal = event.key;
-		textInVal = textIn.value + letterInVal;
+		textInVal = textIn.value //+ letterInVal;
+		letterInVal = textInVal.slice(-1);
 		currentWordLetterNum = textInVal.length - 1;
 
+		if (textInVal.trim() == "") {
+			event.stopPropagation()
+			event.preventDefault()
+			textIn.value = '';
+			return false
+		}//if space is press at the beginning of a word just ignore it
+
+		// Redid the input handling
 		timerStarted == false && (timerStarted = true) && timer.resume();
 
+		checkIfMistakeOccurred =
+			event.inputType != "deleteContentBackward" &&
+			letterInVal != wordsArray[currentWordNum][currentWordLetterNum] &&
+			(textInVal.length <= wordsArray[currentWordNum].length ||
+				(textInVal.length > wordsArray[currentWordNum].length && !mistakesAllowed))
+		//(If backspace is not pressed &&
+		//the letter typed was a mistake &&
+		//we're still typing inside the bounds of the word or
+		//if were not and mistakes are strictly prohibited) only then count as error.
+		if (checkIfMistakeOccurred) {
+			mistakes++;
+			updateMistakes();
+		}
 
 		// If the  end is reached
 		if (checkWordMatch(textInVal, wordsArray[currentWordNum]) && wordsArray.length <= currentWordNum + 1) {
@@ -88,7 +80,7 @@ function addListeners() {
 			timer.stop();
 			timerStarted = false;
 			generateWords();
-		} else if ((event.keyCode == 13 || event.keyCode == 32) && checkWordMatch(textInVal, wordsArray[currentWordNum])) {
+		} else if ((letterInVal == " "  /*if space */ || event.keyCode == 13) && checkWordMatch(textInVal, wordsArray[currentWordNum])) {
 			event.stopPropagation();
 			event.preventDefault();
 			prepareNextWord();
@@ -98,29 +90,52 @@ function addListeners() {
 			}
 			document.querySelector(`word${currentWordNum}`).style.color = "inherit";
 		} else {
-			if (event.key == wordsArray[currentWordNum][currentWordLetterNum]) {
-				currentLetterElement.style.color = correctLetterColor;
-			} else {
-				if (textInVal.length<=wordsArray[currentWordNum].length) {
-					currentLetterElement.style.color = incorrectLetterColor;
+			for (let i = 0; i < wordsArray[currentWordNum].length; i++) {
+				currentLetterElement = getWordLetter(currentWordNum, i);
+				if (i < textInVal.length) {
+					if (textInVal[i] == wordsArray[currentWordNum][i]) {
+						currentLetterElement.style.color = correctLetterColor;
+					}
+					else {
+						currentLetterElement.style.color = incorrectLetterColor;
+						var mistakeEncountered = true;
+					}
+				} else {
+					currentLetterElement.style.color = "";
 				}
-				if (textInVal.length<=wordsArray[currentWordNum].length ||(textInVal.length>wordsArray[currentWordNum].length && !mistakesAllowed)) {
-					
-					textIn.style.background = incorrectLetterColor;
-					mistakes++;
-					updateMistakes();
-
 			}
+			(mistakeEncountered && (textIn.style.background = incorrectLetterColor)) || (textIn.style.background = "");
+		}
+	});
+
+
+	settings_button = document.querySelector('settings_button');
+	settings_button.addEventListener('click', toggleSettings = (event) => {
+		settingsEl = document.querySelector('settings');
+		mainContainerEl = document.querySelector('main_container');
+		var checkId;
+		if (settingsEl.style.display != 'block') {
+			settingsEl.style.display = 'block';
+			mainContainerEl.style.display = 'none'
+
+			document.querySelector("#mistakesAllowed").checked = mistakesAllowed;
+			document.querySelector("#wordListLevel" + wordListLevel).checked = true;
+
+
+		} else {
+			settingsEl.style.display = '';
+			mainContainerEl.style.display = ''
+
+
+			mistakesAllowed = document.querySelector("#mistakesAllowed").checked;
+			wordListLevel = document.querySelector("#wordListLevel0").checked ? 0 : 1;
+
+			cookie.set('set_mistakesAllowed', mistakesAllowed)
+			cookie.set('set_wordListLevel', wordListLevel)
+
+			generateWords()
 		}
 
-			if (elementExists(getWordLetter(currentWordNum, currentWordLetterNum + 1))) {
-				nextWordLetter = getWordLetter(currentWordNum, currentWordLetterNum + 1);
-			} else {
-				document.querySelector(`word${currentWordNum}`).style.borderRightWidth = "1px";
-				document.querySelector(`word${currentWordNum}`).style.color = "inherit";
-			}
-			currentWordLetterNum++;
-		}
 	});
 }
 
@@ -142,11 +157,10 @@ function prepareNextWord(params) {
 	currentWordLetterNum = 0; // Reset the letter number to the first index (0)
 	wordsTyped++;
 	document.querySelector("wpm").innerHTML = Math.round((60 / (timer.timerElapsed / 1000)) * wordsTyped, 2); // Calc wpm
-	textIn.style.backgroundColor="";
+	textIn.style.backgroundColor = "";
 }
 
 function checkWordMatch(a, b) {
-// console.log(a.trim(),b);
 	if (mistakesAllowed) {
 		return a.trim().length >= b.length;
 	} else {
@@ -176,3 +190,21 @@ timer = {
 		}, timer.updateInterval);
 	},
 };
+
+
+
+cookie = {
+
+	set: (name, val) => {
+
+		document.cookie = ` ${name}=${val};path=/`;
+	},
+	get: (name) => {
+		if (document.cookie.trim() == "") return false;
+		return document.cookie.split(name + '=')[1].split(';')[0]
+	},
+	remove: () => {
+		document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+	},
+
+}
